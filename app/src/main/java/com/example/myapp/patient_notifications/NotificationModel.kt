97fdee45c511bddcde51a59a301b8a1d0e82eebd
marylaa <maryla.sylwestrzak@gmail.com>
2023-05-26 20:45:32 +0200

    dzialaja powiadomienia w aplikacji

--- app/src/main/java/com/example/myapp/patient_notifications/NotificationModel.kt
+++ app/src/main/java/com/example/myapp/patient_notifications/NotificationModel.kt
@@ -1,9 +1,8 @@
-package com.example.myapp.patient_notifications
 
-data class NotificationModel (
-    var message: String = "",
-    var pill: String = "",
-    var date: String = "",
-    var pacient: String = "",
+class NotificationModel {
+    var message: String = ""
+    var pill: String = ""
+    var date: String = ""
+    var pacient: String = ""
     var id: String = ""
-)
+}
